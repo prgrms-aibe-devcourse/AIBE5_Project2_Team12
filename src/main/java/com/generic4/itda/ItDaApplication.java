1d3618cb21b11c3467ba3656616d46de9f9b013b
seoilhyeon <ilhyeon0326@gmail.com>
2026-04-06 05:23:47 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/generic4/itda/ItDaApplication.java
@@ -0,0 +1,13 @@
+package com.generic4.itda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItDaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generic4/itda/ItDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic4.itda;
+
+import com.generic4.itda.annotation.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+class ItDaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
