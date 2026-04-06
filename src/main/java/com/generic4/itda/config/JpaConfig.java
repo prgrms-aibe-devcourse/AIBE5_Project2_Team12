96fbcac70aae22920f4e97a0dce1280a0ce81151
seoilhyeon <ilhyeon0326@gmail.com>
2026-04-06 09:09:59 +0900

    feat: JPA Auditing 설정 추가

--- src/main/java/com/generic4/itda/config/JpaConfig.java
+++ src/main/java/com/generic4/itda/config/JpaConfig.java
@@ -1,10 +1,13 @@
 package com.generic4.itda.config;
 
+import com.generic4.itda.dto.security.ItDaPrincipal;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @EnableJpaAuditing
 @Configuration
@@ -12,6 +15,11 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of("dummy");
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .filter(ItDaPrincipal.class::isInstance)
+                .map(ItDaPrincipal.class::cast)
+                .map(ItDaPrincipal::getName);
     }
 }
