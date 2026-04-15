5a5894e8b747156cb260c7c9bed520a56787382b
seoilhyeon <ilhyeon0326@gmail.com>
2026-04-15 10:52:32 +0900

    feat: Proficiency 정렬을 위한 priority 추가

--- src/main/java/com/generic4/itda/domain/resume/Proficiency.java
+++ src/main/java/com/generic4/itda/domain/resume/Proficiency.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum Proficiency {
-    BEGINNER("초급"), INTERMEDIATE("중급"), ADVANCED("고급");
+    BEGINNER("초급", 1), INTERMEDIATE("중급", 2), ADVANCED("고급", 3);
 
     private final String description;
+    private final int priority;
 
-    Proficiency(String description) {
+    Proficiency(String description, int priority) {
         this.description = description;
+        this.priority = priority;
     }
 }
