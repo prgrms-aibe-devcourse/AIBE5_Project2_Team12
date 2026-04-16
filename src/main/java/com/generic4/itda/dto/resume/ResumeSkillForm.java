9d5e34a188b1c3036f1a5ea893564bdc7e8a7c79
gyu98-mun <ansrb537@gmail.com>
2026-04-16 16:14:32 +0900

    feat: 기술 스택 및 숙련도 데이터 전송 객체 추가

---
+++ src/main/java/com/generic4/itda/dto/resume/ResumeSkillForm.java
@@ -0,0 +1,19 @@
+package com.generic4.itda.dto.resume;
+
+import com.generic4.itda.domain.resume.Proficiency;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResumeSkillForm {
+
+    @NotNull(message = "스킬은 필수값입니다.")
+    private Long skillId;
+
+    @NotNull(message = "숙련도는 필수값입니다.")
+    private Proficiency proficiency;
+}
